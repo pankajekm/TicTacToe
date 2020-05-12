185f74e2e2e62a995a952e6f48bd367b827f07a2
Pankaj Kumar G <pankaj.ekm@gmail.com>
2020-05-12 16:34:45 +0530

    base files Added

---
+++ cmake-build-debug/CMakeFiles/TicTacToe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TicTacToe.dir/TicTacToe.cpp.o"
+  "CMakeFiles/TicTacToe.dir/main.cpp.o"
+  "TicTacToe"
+  "TicTacToe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TicTacToe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
